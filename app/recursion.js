83f4d3c07906b9f7081d2b48e54648dd2761e679
Destin2000x <Destin2000x@hotmail.com>
2016-11-07 19:37:23 -0500

    Update recursion.js

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-
+  	if(dirName === data.dir)
+  	{
+  		dirName = undefined;
+  	}
+  	var fileList = [];
+  	for(var i = 0; i < data.files.length; i++)
+  	{
+  		if(typeof data.files[i] === 'object')
+  		{
+  			fileList = fileList.concat(this.listFiles(data.files[i], dirName));
+  		}
+  		else
+  		{
+  			if(typeof dirName == 'undefined')
+  			{
+  				fileList.push(data.files[i]);
+  			}
+  		}
+  	}
+  	return fileList;
   },
 
   permute: function(arr) {
