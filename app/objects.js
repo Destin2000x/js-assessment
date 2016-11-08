ecdafc5d0992ad888db56dd3b3b5eaadd61b1f43
Destin2000x <Destin2000x@hotmail.com>
2016-11-07 19:37:03 -0500

    Update objects.js

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+  	obj.words = fn;
+  	return obj.words();
   },
 
   alterObjects: function(constructor, greeting) {
-
+  	constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+  	var answerArray = [];
+  	for(var prop in obj) {
+  		if(obj.hasOwnProperty(prop))
+  		{
+  			answerArray.push(prop + ': ' + obj[prop]);
+  		}
+  	}
+  	return answerArray;
   }
 };
