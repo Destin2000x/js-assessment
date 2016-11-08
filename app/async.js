8d77ed7bc71958c964c9c9cdb5978ed4c19a4a77
Destin2000x <Destin2000x@hotmail.com>
2016-11-07 19:36:33 -0500

    Update async.js

--- app/async.js
+++ app/async.js
@@ -1,11 +1,26 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
+  async: function(value, time) {
+	   var testDeferred = new jQuery.Deferred();
+	   if(!time)
+	   	var time = 10;
+
+	   setTimeout(function() {
+	       testDeferred.resolve(value);
+	   }, time);
+	   return testDeferred.promise();
 
   },
 
-  manipulateRemoteData: function(url) {
-
-  }
+  	manipulateRemoteData: function(url) {
+		return Promise.resolve($.ajax(url)).then(function (value) {
+		   var res = [];
+		   var people = value.people;
+		   for(var i=0;i<people.length;i++){
+		     res.push(people[i].name);
+		   }
+		   return res.sort();
+	 	})
+	}
 };
